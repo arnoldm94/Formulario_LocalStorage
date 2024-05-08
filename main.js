131e8e7ab6b5ed15a22811de4332564810cd33c0
Arnold Melet <arnoldmelet18@gmail.com>
2024-05-08 20:27:43 +0200

    primero

---
+++ main.js
@@ -0,0 +1,41 @@
+const yourName = document.getElementById ('name');
+const submitBtn = document.getElementById ('submitBtn');
+const yourEmail = document.getElementById ('email');
+const comentario = document.getElementById ('comentario');
+
+
+
+var user
+
+function onSubmit(event){
+    event.preventDefault()
+    const valueNombre = yourName.value
+    const valueEmail = yourEmail.value
+    const valueComentario = comentario.value
+    
+    localStorage.setItem('user', JSON.stringify({
+        Nombre: valueNombre,
+        Email: valueEmail,
+        Comentario: valueComentario
+
+    }))
+    
+    const user = JSON.parse(localStorage.getItem('user'))
+    const h1 = document.createElement("h1")
+    h1.textContent = "se han guardado los datos de: " + user.Nombre
+    document.body.appendChild(h1)
+    
+   
+    
+    /* 
+    localStorage.setItem('Nombre',valueNombre)
+    localStorage.setItem('Email',valueEmail)
+    localStorage.setItem('Comentarios',valueComentario)
+ 
+    localStorage.getItem('Nombre',valueNombre)
+    localStorage.getItem('Email',valueEmail)
+    localStorage.getItem('Comentarios',valueComentario) */
+
+};
+submitBtn.addEventListener("click",onSubmit);
+
